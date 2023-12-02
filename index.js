5ab7a1a9b3e9af20c9fb94c47e704b5759aa8801
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2023-12-02 15:01:41 -0800

    edit text on testimonials page and remove the year end letter section.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const welcomeBtn = document.getElementById('welcome-btn');
 const aboutBtn = document.getElementById('about-btn');
 const testimonialsBtn = document.getElementById("testimonials-btn");
 const searchEngagementsBtn = document.getElementById("search-engagements-btn");
-const yearEndLetterBtn = document.getElementById("year-end-letter-btn");
+//const yearEndLetterBtn = document.getElementById("year-end-letter-btn");
 const contactBtn = document.getElementById("contact-btn");
 
 const abtQualityBtn = document.getElementById('abt-quality-btn');
@@ -13,7 +13,7 @@ const welcomeSection = document.getElementById("welcome-section");
 const aboutSection = document.getElementById("about-section");
 const testimonialsSection = document.getElementById("testimonials-section");
 const searchEngagementsSection = document.getElementById("search-engagements-section");
-const yearEndLetterSection = document.getElementById("year-end-letter-section");
+//const yearEndLetterSection = document.getElementById("year-end-letter-section");
 const contactInformationSection = document.getElementById("contact-information-section");
 
 const abtSectionDefault = document.getElementById('about-section-default');
@@ -21,9 +21,9 @@ const abtQualitySection = document.getElementById('abt-quality-section');
 const abtEthicsSection = document.getElementById('abt-ethics-section');
 const abtBackgroundsSection = document.getElementById('abt-backgrounds-section');
 
-const sectionArray = [welcomeSection, aboutSection, testimonialsSection, searchEngagementsSection, yearEndLetterSection, contactInformationSection];
+const sectionArray = [welcomeSection, aboutSection, testimonialsSection, searchEngagementsSection, contactInformationSection];
 const abtSectionArray = [abtSectionDefault, abtQualitySection, abtEthicsSection, abtBackgroundsSection];
-const mainNavBtnArray = [aboutBtn, testimonialsBtn, searchEngagementsBtn, yearEndLetterBtn];
+const mainNavBtnArray = [aboutBtn, testimonialsBtn, searchEngagementsBtn];
 
 const navyBlue = '#112557';
 const ltGrey = '#919AB1';
@@ -103,25 +103,25 @@ searchEngagementsBtn.addEventListener("click", function(e){
     });  
 });
 
-yearEndLetterBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    sectionArray.forEach(element => {
-        if (element.id != "year-end-letter-section") {
-            element.style.display = "none"
+// yearEndLetterBtn.addEventListener("click", function(e){
+//     e.preventDefault();
+//     sectionArray.forEach(element => {
+//         if (element.id != "year-end-letter-section") {
+//             element.style.display = "none"
             
-        } else {
-            element.style.display = "block"
-            yearEndLetterBtn.style.color = "#919AB1";
-        }
-    });
-    mainNavBtnArray.forEach(btn => {
-        if (btn.id == "year-end-letter-btn"){
-            btn.style.color = ltGrey;
-        } else {
-            btn.style.color = navyBlue;
-        }
-    });  
-});
+//         } else {
+//             element.style.display = "block"
+//             yearEndLetterBtn.style.color = "#919AB1";
+//         }
+//     });
+//     mainNavBtnArray.forEach(btn => {
+//         if (btn.id == "year-end-letter-btn"){
+//             btn.style.color = ltGrey;
+//         } else {
+//             btn.style.color = navyBlue;
+//         }
+//     });  
+// });
 
 contactBtn.addEventListener("click", function(e){
     e.preventDefault();
